8e329dc27e9308dbb6a9b9e31cc09e4ca93a534f
Armando Sánchez Trabajando <32597371+Programadores-en-desarrollo@users.noreply.github.com>
2023-08-26 21:58:35 -0600

    Update principal.js

--- js/principal.js
+++ js/principal.js
@@ -1,1 +1,25 @@
-
+var app = new Vue({
+    el : '#app',
+    data : {
+        lista : [
+        ],
+        nombre : '',
+        promedio : ''
+    },
+    methods : {
+        agregarNota: function (){
+            estadoA=false;
+            if (this.promedio>=7){
+                estadoA=true;
+            }
+            if (this.nombre!="" && this.promedio!=""){
+                this.lista.push({nombre:this.nombre, promedio:this.promedio, estado:estadoA});
+                this.nombre="";
+                this.promedio="";
+            }
+            else{
+                alert ("Ingrese el nombre y promedio del estudiante");
+            }
+        }
+    }
+})
